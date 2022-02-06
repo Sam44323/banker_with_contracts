887499deeed057a71085aa19ab40d13f517eba1b
Suranjan <suranjanbiswas2002@gmail.com>
2022-02-06 21:47:44 +0530

    add: added initial test file banking

---
+++ test/unit/Banking.ts
@@ -0,0 +1,6 @@
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+import { expect } from "chai";
+import { ethers } from "hardhat";
+import { Banking } from "../../typechain";
+
+describe("Banking.sol test", () => {});
