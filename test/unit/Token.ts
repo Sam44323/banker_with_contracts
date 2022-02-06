1d71c91bb2a08efebc9ecd71f580ef8d64b0feae
Suranjan <suranjanbiswas2002@gmail.com>
2022-02-06 16:25:57 +0530

    add: added test for checking the current owner of token_contract

--- test/unit/Token.ts
+++ test/unit/Token.ts
@@ -5,4 +5,16 @@ import { Token } from "../../typechain";
 
 describe("Testing the BKR Token", () => {
   let token: Token, owner: SignerWithAddress, recipient: SignerWithAddress;
+
+  beforeEach(async () => {
+    [owner, recipient] = await ethers.getSigners();
+    const TokenFactory = ethers.getContractFactory("Token");
+    token = await (await TokenFactory).connect(owner).deploy();
+
+    console.log("Deployed the token");
+  });
+
+  it("Checks the admin of the token contract", async () => {
+    expect(await token.owner()).to.equal(owner.address);
+  });
 });
