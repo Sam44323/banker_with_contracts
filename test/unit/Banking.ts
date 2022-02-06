ebf4e881d3e7d3f74957725834d359aa299fcd45
Suranjan <suranjanbiswas2002@gmail.com>
2022-02-06 21:53:58 +0530

    add: added suite requirement initiater for banking_test

--- test/unit/Banking.ts
+++ test/unit/Banking.ts
@@ -1,6 +1,25 @@
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { expect } from "chai";
 import { ethers } from "hardhat";
-import { Banking } from "../../typechain";
+import { Banking, Token } from "../../typechain";
 
-describe("Banking.sol test", () => {});
+describe("Banking.sol test", () => {
+  let banking: Banking,
+    token: Token,
+    owner: SignerWithAddress,
+    userA: SignerWithAddress,
+    userB: SignerWithAddress,
+    userC: SignerWithAddress;
+
+  beforeEach(async () => {
+    [owner, userA, userB, userC] = await ethers.getSigners();
+
+    token = await (await ethers.getContractFactory("Token"))
+      .connect(owner)
+      .deploy();
+
+    banking = await (await ethers.getContractFactory("Banking"))
+      .connect(owner)
+      .deploy(token.address);
+  });
+});
